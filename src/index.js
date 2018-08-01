5532b2fa129961cbc18fd3be0b3f9c647b1bdf17
Fabien Lamarque <flamarque@octo.com>
2018-08-01 22:57:43 +0200

    first commit of tutorial

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react"
+import ReactDOM from "react-dom"
+import SearchBar from "./components/search_bar"
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyD46zrm7vSd6zHLdLuW-sh5_PSqlxnT83k";
+SearchBar.valueOf();
+const App = () => <div><SearchBar/></div>;
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));
