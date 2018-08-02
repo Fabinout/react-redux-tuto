7fa821e42c4b05dccd24527fa0f440c2530748bb
fabien lamarque <fabien.lamarque@ext.cdiscount.com>
2018-08-02 13:11:40 +0200

    activated a youtube search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import SearchBar from "./components/search_bar"
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = "AIzaSyD46zrm7vSd6zHLdLuW-sh5_PSqlxnT83k";
-SearchBar.valueOf();
+
+YTSearch({key: API_KEY, term: "react redux"}, function (data) {
+    console.log(data)
+});
+
 const App = () => <div><SearchBar/></div>;
 
 
