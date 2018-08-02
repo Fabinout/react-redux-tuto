312548d4848e586e70e9341a31229c8f4e0537a7
fabien lamarque <fabien.lamarque@ext.cdiscount.com>
2018-08-02 13:19:48 +0200

    added youtube fetch on startup,

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from "react"
+import React, {Component} from "react"
 import ReactDOM from "react-dom"
 import SearchBar from "./components/search_bar"
 import YTSearch from 'youtube-api-search'
 
 const API_KEY = "AIzaSyD46zrm7vSd6zHLdLuW-sh5_PSqlxnT83k";
 
-YTSearch({key: API_KEY, term: "react redux"}, function (data) {
-    console.log(data)
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
 
-const App = () => <div><SearchBar/></div>;
+        YTSearch({key: API_KEY, term: "react redux"}, videos => {
+            this.setState({videos});
+        });
+    }
 
+    render() {
+        return <div><SearchBar/></div>;
+    }
+}
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
