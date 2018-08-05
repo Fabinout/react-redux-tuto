6dceee54536f80a91d996557f06a8a59b7ef7807
Fabien Lamarque <flamarque@octo.com>
2018-08-05 16:52:06 +0200

    advanced a bit in the tutorial

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom"
 import SearchBar from "./components/search_bar"
 import YTSearch from 'youtube-api-search'
 import VideoList from './components/video_list'
+import VideoDetail from "./components/video_detail"
 
 const API_KEY = "AIzaSyD46zrm7vSd6zHLdLuW-sh5_PSqlxnT83k";
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {videos: []};
-
+        this.state = {videos: [], selectedVideo: null};
         YTSearch({key: API_KEY, term: "react redux"}, videos => {
-            this.setState({videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -20,7 +23,9 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList videos={this.state.videos}
+                           onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
             </div>
         );
     }
